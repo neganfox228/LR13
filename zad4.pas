78821bed753caa0b73864664dccff2435b050ff9
neganfox228 <117731124+neganfox228@users.noreply.github.com>
2022-12-14 10:42:54 +0300

    Add files via upload

---
+++ zad4.pas
@@ -0,0 +1,18 @@
+﻿var
+intext,outtext: text;
+s: string;
+k,i: integer;
+begin
+  Writeln('Введите переменную');
+  readln(k);
+  assign(intext, '4text.txt');
+  assign(outtext,'4text1.txt');
+  reset(intext);
+  rewrite(outtext);
+  i:=0;
+  write(outtext,k);
+  readln(intext,s);
+    write(outtext,s);
+  close(intext);
+  close(outtext);
+end.
---
+++ zad7.pas
@@ -0,0 +1,19 @@
+﻿begin 
+  var int,out:text;
+  assign(int, 'z7.in.txt');
+  assign(out, 'z7.out.txt');
+  reset(int);
+  rewrite(out);
+  var r:integer;
+  read(int,r);
+  close(int);
+  var sum:integer;
+  var k:integer;
+  for var i:=1 to r do begin 
+    k:=0;
+    for var j:=1 to i do if i mod j = 0 then inc(k);
+    if k=5 then sum+=i;
+  end;
+  print(out,sum);
+  close(out);
+end.
